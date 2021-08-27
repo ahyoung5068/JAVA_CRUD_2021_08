16ab9c391d5b5b45e03c81b5d65fe0aee53f6b4b
ahyoung5068 <dkdud5068@gmail.com>
2021-08-27 17:48:20 +0900

    게시물 작성시 작성날짜 저장

--- src/com/sbs/java/crud/Main.java
+++ src/com/sbs/java/crud/Main.java
@@ -3,17 +3,14 @@ package com.sbs.java.crud;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("==ÇÁ·Î±×·¥ ½ÃÀÛ==");
+		System.out.println("==í”„ë¡œê·¸ë¨ ì‹œì‘==");
 		Scanner sc = new Scanner(System.in);
 		int lastArticleId = 0;
-
 		List<Article> articles = new ArrayList<>();
-
 		while (true) {
-			System.out.printf("¸í·É¾î) ");
+			System.out.printf("ëª…ë ¹ì–´) ");
 			String command = sc.nextLine().trim();
 			if (command.length() == 0) {
 				continue;
@@ -24,95 +21,87 @@ public class Main {
 			if (command.equals("article write")) {
 				int id = lastArticleId + 1;
 				lastArticleId = id;
-				System.out.printf("Á¦¸ñ : ");
+				String regDate = Util.getNowDateStr();
+				System.out.printf("ì œëª© : ");
 				String title = sc.nextLine();
-				System.out.printf("³»¿ë : ");
+				System.out.printf("ë‚´ìš© : ");
 				String body = sc.nextLine();
-
-				Article article = new Article(id, title, body);
+				
+				Article article = new Article(id, regDate, title, body);
 				articles.add(article);
 
-				System.out.printf("%d¹ø ±ÛÀÌ »ı¼ºµÇ¾ú½À´Ï´Ù.\n", id);
+				System.out.printf("%dë²ˆ ê¸€ì´ ìƒì„±ë˜ì—ˆìŠµë‹ˆë‹¤.\n", id);
 			} else if (command.equals("article list")) {
 				if (articles.size() == 0) {
-					System.out.println("°Ô½Ã¹°ÀÌ ¾ø½À´Ï´Ù.");
+					System.out.println("ê²Œì‹œë¬¼ì´ ì—†ìŠµë‹ˆë‹¤.");
 					continue;
 				}
-				System.out.println("¹øÈ£  /  Á¦¸ñ");
+				System.out.println("ë²ˆí˜¸  /  ì œëª©");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-
 					System.out.printf("%d  /  %s\n", article.id, article.title);
 				}
-
 			} else if (command.startsWith("article detail ")) {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]); // "1" -> 1
-
 				Article foundArticle = null;
-
 				for (int i = 0; i < articles.size(); i++) { // 0 1 2 3 4
 					Article article = articles.get(i);
-
 					if (article.id == id) {
 						foundArticle = article;
 						break;
 					}
 				}
-
 				if (foundArticle == null) {
-					System.out.printf("%d¹ø °Ô½Ã¹°Àº Á¸ÀçÇÏÁö ¾Ê½À´Ï´Ù.\n", id);
+					System.out.printf("%dë²ˆ ê²Œì‹œë¬¼ì€ ì¡´ì¬í•˜ì§€ ì•ŠìŠµë‹ˆë‹¤.\n", id);
 					continue;
 				}
-				System.out.printf("¹øÈ£ : %d\n", foundArticle.id);
-				System.out.printf("³¯Â¥ : 2021-12-12 12:12:12\n");
-				System.out.printf("Á¦¸ñ : %s\n", foundArticle.title);
-				System.out.printf("³»¿ë : %s\n", foundArticle.body);
+				System.out.printf("ë²ˆí˜¸ : %d\n", foundArticle.id);
+				System.out.printf("ë‚ ì§œ : 2021-12-12 12:12:12\n");
+				System.out.printf("ë‚ ì§œ : %s\n", foundArticle.regDate);
+				System.out.printf("ì œëª© : %s\n", foundArticle.title);
+				System.out.printf("ë‚´ìš© : %s\n", foundArticle.body);
 			} else if (command.startsWith("article delete ")) {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
-
 				int foundIndex = -1;
-
 				for (int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
-
 					if (article.id == id) {
 						foundIndex = i;
 						break;
 					}
 				}
-
 				if (foundIndex == -1) {
-					System.out.printf("%d¹ø °Ô½Ã¹°Àº Á¸ÀçÇÏÁö ¾Ê½À´Ï´Ù.\n", id);
+					System.out.printf("%dë²ˆ ê²Œì‹œë¬¼ì€ ì¡´ì¬í•˜ì§€ ì•ŠìŠµë‹ˆë‹¤.\n", id);
 					continue;
 				}
 				// size() => 3
 				// index : 0 1 2
 				// id : 1 2 3
 				articles.remove(foundIndex);
-				System.out.printf("%d¹ø °Ô½Ã¹°ÀÌ »èÁ¦µÇ¾ú½À´Ï´Ù.\n", id);
+				System.out.printf("%dë²ˆ ê²Œì‹œë¬¼ì´ ì‚­ì œë˜ì—ˆìŠµë‹ˆë‹¤.\n", id);
 			}
-
 			else {
-				System.out.printf("%s(Àº)´Â Á¸ÀçÇÏÁö ¾Ê´Â ¸í·É¾îÀÔ´Ï´Ù.\n", command);
+				System.out.printf("%s(ì€)ëŠ” ì¡´ì¬í•˜ì§€ ì•ŠëŠ” ëª…ë ¹ì–´ì…ë‹ˆë‹¤.\n", command);
 			}
 		}
-
 		sc.close();
-
-		System.out.println("==ÇÁ·Î±×·¥ ³¡==");
+		System.out.println("==í”„ë¡œê·¸ë¨ ë==");
 	}
 }
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 
-	public Article(int id, String title, String body) {
+	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
+		
 	}
 }
